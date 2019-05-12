5a4d3c7af5837d948e9bcdd146addc922ee3a62e
hellojukay <hellojukay@163.com>
2019-05-12 11:57:15 +0800

    init framework

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/hellojukay/git-ignore
 
-require gopkg.in/AlecAivazis/survey.v1 v1.8.4
+require (
+	github.com/urfave/cli v1.20.0
+	golang.org/x/tools v0.0.0-20190509014725-d996b19ee77c // indirect
+	gopkg.in/AlecAivazis/survey.v1 v1.8.4
+)
--- main.go
+++ main.go
@@ -1,47 +1,49 @@
 package main
 
 import (
-	"flag"
-	"gopkg.in/AlecAivazis/survey.v1"
-	"strings"
+	"os"
+	"fmt"
+	"github.com/urfave/cli"
 )
 
 // is init ?
 var i bool
-
+var app *cli.App
 func init() {
-	if !flag.Parsed() {
-		flag.Parse()
+	app = cli.NewApp()
+	app.Commands = []cli.Command{
+		{
+		  Name:    "init",
+		  Aliases: []string{"i"},
+		  Usage:   "init git ignore file",
+		  Action:  func(c *cli.Context) error {
+			fmt.Println("added task: ", c.Args().First())
+			return nil
+		  },
+		  BashComplete: initAutoComplete,
+		},
+		{
+		  Name:    "reset",
+		  Usage:   "complete a task on the list",
+		  Action:  func(c *cli.Context) error {
+			fmt.Println("completed task: ", c.Args().First())
+			return nil
+		  },
+		},
 	}
-	flag.BoolVar(&i, "i", false, "init and reset gitignore file")
+	app.Action = func(c *cli.Context)error {
+		println("Hello World")
+		return nil
+	}
+	app.EnableBashCompletion = true
+	app.BashComplete = func(c *cli.Context) {
+		println("Hello World\n")
+	}
+}
+
+func initAutoComplete(c *cli.Context)  {
+	fmt.Fprintf(c.App.Writer,"Hello \n World")
 }
 func main() {
-
-	// TODO 获取 gitignore list
-	// TODO gitignore list to array
-	// parse args
-	// switch
-	// init
-	var items = getItems()
-	var values []string
-	promt := survey.MultiSelect{
-		Message: "Choose items:",
-		Options: items,
-		FilterFn:filter,
-		PageSize:5,
-		}
-		survey.AskOne(&promt,&values,nil)
-}
-
-func filter(filter string, options []string)[]string  {
-	if len(options) == 0 {
-		return options
-	}
-	var values []string
-	for index := range options {
-		if strings.Contains(options[index],filter) {
-			values = append(values,options[index])
-		}
-	}
-	return values
+	app.Run(os.Args)
 }
